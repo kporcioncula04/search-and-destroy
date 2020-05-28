7007ebc7510c6ccf917930ebd8f6c0c89470de8c
Karolina <kporcioncula.04@gmail.com>
2020-05-28 17:55:02 -0400

    algos

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,13 @@
 
 // Complete this algo
 const minJumps = arr => {
+  let count = 0; // to store the total count
+  let initalVal = arr[0]
+  let currVal;
 
+  // while(initalVal > 0){
+
+  // }
 };
 
 module.exports = minJumps
